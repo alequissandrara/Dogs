33c9764bff290c687c15ef83b3b08314e409803e
alequissandrara <alequissandrarayane123@gmail.com>
2023-10-31 17:15:04 -0300

    Login Criar

---
+++ src/Hooks/useFetch.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  return {
+    data,
+    loading,
+    error,
+  };
+};
+
+export default useFetch;
