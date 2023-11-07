f42aaa8b3ed8e1f079ed49cba829eff1180c8634
alequissandrara <alequissandrarayane123@gmail.com>
2023-11-07 15:12:37 -0300

    Responsividade do Menu para mobile e finalização da página de  Postagem para fotos.

--- src/Componentes/User/UserHeaderNav.jsx
+++ src/Componentes/User/UserHeaderNav.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { NavLink, Navigate, useNavigate } from "react-router-dom";
+import { NavLink, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 import MinhasFotos from "../../Assets/feed.svg";
 import Estatisticas from "../../Assets/estatisticas.svg";
 import Adicionar from "../../Assets/adicionar.svg";
 import Sair from "../../Assets/sair.svg";
 import styles from "./UserHeaderNav.module.css";
+import UseMedia from "../../Hooks/UseMedia";
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
 
   function handleLogout() {
@@ -16,25 +16,49 @@ const UserHeaderNav = () => {
     navigate = "/login";
   }
 
+  const mobile = UseMedia("(max-width: 40rem)");
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/conta" end>
-        <img src={MinhasFotos} alt="Icon MinhasFotos" />
-        {mobile && "Minhas Fotos"}
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <img src={Estatisticas} alt="Icon Estatisticas" />
-        {mobile && "Estatisticas"}
-      </NavLink>
-      <NavLink to="/conta/postar">
-        <img src={Adicionar} alt="Icon Adicionar" />
-        {mobile && "Adicionar"}
-      </NavLink>
-      <button onClick={handleLogout}>
-        <img src={Sair} alt="Icon Sair" />
-        {mobile && "Sair"}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to="/conta" end>
+          <img src={MinhasFotos} alt="Icon MinhasFotos" />
+          {mobile && "Minhas Fotos"}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <img src={Estatisticas} alt="Icon Estatisticas" />
+          {mobile && "Estatisticas"}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <img src={Adicionar} alt="Icon Adicionar" />
+          {mobile && "Adicionar"}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <img src={Sair} alt="Icon Sair" />
+          {mobile && "Sair"}
+        </button>
+      </nav>
+    </>
   );
 };
 
--- src/Hooks/useForms.jsx
+++ src/Hooks/useForms.jsx
@@ -10,11 +10,11 @@ const types = {
     regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
     message:
       "A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.",
-  } /*
+  },
   number: {
     regex: /^\d+$/,
-    message: 'Utilize números apenas.',
-  },*/,
+    message: "Utilize números apenas.",
+  },
 };
 
 const useForm = (type) => {
