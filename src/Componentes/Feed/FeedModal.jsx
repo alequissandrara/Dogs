77014ea1dfdd18512647d3c745c371436527e8a2
alequissandrara <alequissandrarayane123@gmail.com>
2023-11-10 16:41:17 -0300

    Implementação do Feed Modal

--- src/Componentes/Feed/FeedModal.jsx
+++ src/Componentes/Feed/FeedModal.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "./FeedModal.module.css";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTO_GET } from "../../api";
+import Loading from "../Helper/Loading";
+import PhotoContent from "../Photo/PhotoContent";
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) {
+      return setModalPhoto(null);
+    }
+  }
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <p className={styles.error}>{error}</p>}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
--- src/Componentes/Feed/FeedPhotosItem.jsx
+++ src/Componentes/Feed/FeedPhotosItem.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./FeedPhotosItem.module.css";
 
-const FeedPhotosItem = ({ photo }) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   // Verifica se a propriedade 'photo' é definida
   if (!photo) {
     return null; // Ou qualquer outra renderização ou tratamento desejado
   }
 
+  function handleClick() {
+    setModalPhoto(photo);
+  }
+
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={handleClick}>
       {/* Verifica se 'photo.src' é definido antes de acessá-lo */}
       {photo.src && <img src={photo.src} alt={photo.title} />}
       <span className={styles.visualizacao}>{photo.acessos}</span>
