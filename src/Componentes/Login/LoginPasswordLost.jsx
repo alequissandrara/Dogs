4cbdd1444d271549b8a7d4df9c150ef3362b71ed
alequissandrara <alequissandrarayane123@gmail.com>
2023-12-18 21:41:22 -0300

    Configurando o Password Lost

--- src/Componentes/Login/LoginPasswordLost.jsx
+++ src/Componentes/Login/LoginPasswordLost.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForms";
+import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_LOST } from "../../api";
 
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = PASSWORD_LOST({
+      login: login.value,
+      url: window.location.href.replace("perdeu", "resetar"),
+    });
+    await request(url, options);
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: "#4c1" }}> {data} </p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email/ Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+      {error && <p className={styles.error}>Tente novamente!</p>}
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
