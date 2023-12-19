75405955e57f3bc9a08c0e8530dbc4a0eeacc72c
alequissandrara <alequissandrarayane123@gmail.com>
2023-12-19 00:21:51 -0300

    alterando o desingn do Footer

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import dogFooter from "../Assets/dogs-footer.svg";
 
 const Footer = () => {
-  return <div className={styles.footer}>eu sou o Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <img src={dogFooter} />
+        Dogs. Alguns direitos reservados.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
